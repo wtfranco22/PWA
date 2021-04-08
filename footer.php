58ec65f06d2f2c73e619408aee7406c39e2f184e
wtfranco22 <francofranco_96@hotmail.com>
2021-04-08 06:13:32 -0300

    un script exclusivo para el pie de la pagina y ahorramos codigo repetido

---
+++ footer.php
@@ -0,0 +1,58 @@
+    <div class="container-fluid c1 pie">
+        <div class="row justify-content-center">
+            <div class="col col-6 col-md-4">
+                <p class="text-center">
+                    Escuelas de Parkour-Freerunning
+                </p>
+                <nav>
+                    <ul>
+                        <li>
+                            <a class="nav-link text-muted" target="_blank" href="https://www.facebook.com/Club-Andino-Villa-La-Angostura-134010936629558/">
+                                Club Andino (Villa la Angostura)
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-muted" target="_blank" href="https://parkourintegral.com/">
+                                Escuela integral de parkour (Bs. As)
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-muted" target="_blank" href="https://www.tempestacademy.com/">
+                                Tempest academy (EE.UU)
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-muted" target="_blank" href="https://3run.co.uk/academy/">
+                                3Run (UK)
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col col-6 col-md-4">
+                <p class="text-center">
+                    Desarrollador
+                </p>
+                <p>
+                    <i class="bi bi-github"></i>
+                    <a class="text-muted" href="https://github.com/wtfranco22">
+                    Wtfranco22
+                    </a>
+                </p>
+                <p>
+                    <i class="bi bi-envelope"></i> franco.rodriguez
+                    <br>@est.fi.uncoma.edu.ar
+                </p>
+                <p>
+                    <i class="bi bi-discord"></i> Wtfranco22 #3497
+                    </a>
+                </p>
+            </div>
+            <p class="text-center">
+                Creative Commons (CC) <a class="text-muted" href="#">parkour.com</a> 2021. Proyecto de software libre y código abierto
+            </p>
+        </div>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
