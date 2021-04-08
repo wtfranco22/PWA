72f94360d47862958c66004834e7c0bb09173eda
wtfranco22 <francofranco_96@hotmail.com>
2021-04-08 06:11:48 -0300

    para no repetir codigo, se creo un script que se encargara de la navegacion dentro de la pagina

---
+++ encabezado.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fotografias.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Gabriela&display=swap" rel="stylesheet">
+    <title>Parkour</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./index.php">Parkour</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./ejercicio2.php">Ejercicio 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./ejercicio4.php">Ejercicio 4</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./ejercicio5.php">Ejercicio 5</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./ejercicio6.php">Ejercicio 6</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
