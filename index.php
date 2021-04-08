2f7fe6a531818fb72dd4be464c349f2f98e3b800
wtfranco22 <francofranco_96@hotmail.com>
2021-04-08 06:15:41 -0300

    variamos la fuente y tamaño, conservamos contenido de texto e imagen

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include_once("./encabezado.php"); ?>
+    <div class="container-fluid c1">
+        <div class="container text-center imgfondo">
+            <div class="d-flex justify-content-center align-items-center min-vh-100">
+                <div>
+                    <h1 class="pb-3">¿Qué es el parkour?</h1>
+                    <h5 class="mt-5">El parkour, es una disciplina física basada en la capacidad motriz del individuo, desarrollado a partir del método natural. Los practicantes son denominados traceurs/traceuses y tienen como objetivo trasladarse de un punto a otro del
+                        entorno de la manera más sencilla y eficiente posible, adaptándose a las exigencias del mismo con la sola ayuda de su cuerpo. Además, se puede considerar una forma de expresión en cuanto a la libertad, el contacto con todo medio
+                        externo y el dominio sobre el propio movimiento natural de cada individuo.El parkour se practica tanto individual como colectivo en cualquier entorno, no exclusivamente en espacios urbanos.
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="container historia p-md-5 min-vh-100">
+            <h1>Historia</h1>
+            <hr>
+            <div class="d-none d-md-block">
+                <img src="galeria/historia.jpg" alt="metodo natural de preparacion fisica">
+            </div>
+            <p class="p-3">El precursor Georges Hébert, que antes de la Primera Guerra Mundial promovió una serie de habilidades atléticas basadas en las que observó de las tribus indígenas que conoció en África.
+            </p>
+            <p class="p-3"> Hébert anotó: <i>"sus cuerpos eran espléndidos, flexibles, ágiles, hábiles, exactos y resistentes, sin más entrenador en la gimnasia que su vida en la naturaleza"</i>. En 1902 hubo una catastrófica erupción volcánica, esta
+                experiencia reforzo su creencia en que la habilidad atlética debe ser combinada con coraje y alturismo-"<i>Ser fuerte para ser útil</i>"
+            </p>
+            <p class="p-3 mb-0">
+                Posteriormente se convirtió en profesor de educación física en la universidad de Reims, en Francia donde estableció un nuevo método de entrenamiento, el méthode naturelle (método natural) consistente en diez grupos fundamentales: caminar, correr, saltar,
+                movimiento cuadrúpedo, escalada, equilibrio, lanzar, levantar, defensa personal y natación. Estos tenían la intención de desarrollar "las tres fuerzas principales": Energética (fuerza de voluntad, coraje, serenidad y firmeza), moral (benevolencia,
+                asistencia, honor y honestidad) y física (músculos y respiración)
+            </p>
+        </div>
+    </div>
+<?php include_once("./footer.php"); ?>
